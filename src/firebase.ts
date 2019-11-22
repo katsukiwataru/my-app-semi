30f93f2c910d03fa77f8e80789d89af4a9d1da18
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-11-22 20:38:31 +0900

    feat: add firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,13 @@
 import firebase from 'firebase/app';
 import App from './appSettings';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = App;
 
 firebase.initializeApp(config);
 
+const firestore = firebase.firestore();
+
 export default firebase;
+export { firestore };
