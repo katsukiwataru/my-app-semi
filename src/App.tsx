604e226d597df38421903e308f082b08b6b37b00
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-11-22 20:38:13 +0900

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Login from './container/login';
+// import Post from './container/post';
+import Post from './container/post';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './plugins/history';
+import { UserProvider } from './context/userContext';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router history={history}>
+      <UserProvider>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/post" component={Post} />
+        </Switch>
+      </UserProvider>
+    </Router>
   );
 };
 
