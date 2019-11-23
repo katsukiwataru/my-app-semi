c0ea506be7d180d46cf35cad67345eed38f0f904
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-11-23 11:39:25 +0900

    feat: add deta

--- src/container/login.tsx
+++ src/container/login.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import firebase from '../firebase';
+import React, { useEffect, useState } from 'react';
+import firebase, { firestore } from '../firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import useUserContext from '../context/userContext';
 
 const Login: React.FC = () => {
+  const [PostData, setPostData] = useState<PostData[]>([]);
   const { user } = useUserContext();
   const uiConfig = {
     signInSuccessUrl: '/',
     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.TwitterAuthProvider.PROVIDER_ID],
   };
 
-  return <div>{!user && <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />}</div>;
+  useEffect(() => {
+    const getPostData = async () => {
+      try {
+        const querySnapshot: any = await firestore.collection('post').get();
+        const records = querySnapshot.docs.map((elem: any) => elem.data());
+        setPostData(records);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPostData();
+  });
+
+  return (
+    <div>
+      <div>{!user && <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />}</div>
+      {PostData.map((item, index) => {
+        return (
+          <React.Fragment key={index}>
+            <p>{item.id}</p>
+          </React.Fragment>
+        );
+      })}
+      <div></div>
+    </div>
+  );
 };
 
 export default Login;
--- src/container/post.tsx
+++ src/container/post.tsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { firestore } from '../firebase';
 import useUserContext from '../context/userContext';
 import history from '../plugins/history';
+import styled from 'styled-components';
 
 const Post: React.FC = () => {
   const { user } = useUserContext();
   const [text, setText] = useState<string>('');
-
-  console.log(user);
 
   if (!user) {
     history.push(`/`);
@@ -17,13 +16,14 @@ const Post: React.FC = () => {
   const sendInputValue = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       try {
-        const responsePost = await firestore
-          .collection('user')
+        await firestore
+          .collection('post')
           .doc()
           .set({
-            name: text,
+            text: text,
+            id: user.uid,
           });
-        console.log(responsePost);
+        history.push(`/`);
       } catch (error) {
         console.log(error);
       }
@@ -32,10 +32,10 @@ const Post: React.FC = () => {
 
   return (
     <div>
-      <input
+      <Input
         type="text"
         value={text}
-        placeholder="Search…"
+        placeholder="コツの掴み方"
         onKeyPress={(e) => sendInputValue(e as React.KeyboardEvent<HTMLInputElement>)}
         onChange={(e) => {
           setText(e.target.value);
@@ -44,5 +44,16 @@ const Post: React.FC = () => {
     </div>
   );
 };
+
+const Input = styled.input`
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 300px;
+  border: none;
+  border-bottom: 1px solid #757575;
+  outline: none;
+  margin: 10rem auto;
+`;
 
 export default Post;
